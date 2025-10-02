af781e9caf170a9487b0b0c7fddcf0f108d1e4b0
mariasmolina <mariia.smolina@gmail.com>
2025-10-02 16:27:51 +0300

    RealEstateIndex

--- ShopTARgv24/ShopTARgv24/Controllers/RealEstateController.cs
+++ ShopTARgv24/ShopTARgv24/Controllers/RealEstateController.cs
@@ -1,22 +1,38 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ShopTARgv24.Core.ServiceInterface;
 using ShopTARgv24.Data;
+using ShopTARgv24.Models.RealEstate;
 
 namespace ShopTARgv24.Controllers
 {
     public class RealEstateController : Controller
     {
         private readonly ShopTARgv24Context _context;
+        private readonly IRealEstateServices _realestateServices;
 
         public RealEstateController
             (
-                ShopTARgv24Context context
+                ShopTARgv24Context context,
+                IRealEstateServices realestateServices
+
             )
         {
             _context = context;
+            _realestateServices = realestateServices;
         }
         public IActionResult Index()
         {
-            return View();
+            var result = _context.RealEstate
+                .Select(x => new RealEstateIndexViewModel
+                {
+                    Id = x.Id,
+                    Area = x.Area,
+                    Location = x.Location,
+                    RoomNumber = x.RoomNumber,
+                    BuildingType = x.BuildingType
+                });
+
+            return View(result);
         }
     }
 }
